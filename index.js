39b2620d2b253848fd47fd92afa106179ef26011
fdsyghuij <msjcagdarodruiz@hotmail.com>
2024-05-11 21:16:52 +0700

    feat task

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const countingSort = (arr) => {
-  const min = Math.min(...arr);
-  const max = Math.max(...arr);
-  const count = Array(max - min + 1).fill(0);
-  for (let num of arr) {
-    count[num - min]++;
-  }
-  let sortedIndex = 0;
-  for (let i = min; i <= max; i++) {
-    while (count[i - min] > 0) {
-      arr[sortedIndex++] = i;
-      count[i - min]--;
+function maximalSquare(matrix) {
+  if (matrix.length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from(Array(rows + 1), () => Array(cols + 1).fill(0));
+  let maxSide = 0;
+  for (let i = 1; i <= rows; i++) {
+    for (let j = 1; j <= cols; j++) {
+      if (matrix[i - 1][j - 1] === "1") {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+        maxSide = Math.max(maxSide, dp[i][j]);
+      }
     }
   }
-  return arr;
-};
+  return maxSide * maxSide;
+}
