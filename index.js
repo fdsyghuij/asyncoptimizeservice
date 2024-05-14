df9eb778953e7aeaf80f7e3e947e9cd450036dbb
fdsyghuij <msjcagdarodruiz@hotmail.com>
2024-05-14 17:34:40 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-function longestPalindromeSubseq(s) {
-  const n = s.length;
-  const dp = Array.from(Array(n), () => Array(n).fill(0));
-  for (let i = 0; i < n; i++) {
-    dp[i][i] = 1;
+function removeNthFromEnd(head, n) {
+  const dummy = new ListNode(0);
+  dummy.next = head;
+  let first = dummy;
+  let second = dummy;
+  for (let i = 0; i <= n; i++) {
+    first = first.next;
   }
-  for (let len = 2; len <= n; len++) {
-    for (let i = 0; i < n - len + 1; i++) {
-      const j = i + len - 1;
-      if (s[i] === s[j]) {
-        dp[i][j] = 2 + dp[i + 1][j - 1];
-      } else {
-        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-      }
-    }
+  while (first !== null) {
+    first = first.next;
+    second = second.next;
   }
-  return dp[0][n - 1];
+  second.next = second.next.next;
+  return dummy.next;
 }
