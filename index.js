0f64214ae85463b6f752e99c9183175d05381c4d
fdsyghuij <msjcagdarodruiz@hotmail.com>
2024-05-17 10:16:48 +0700

    demo project

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-function canCross(stones) {
-  const map = new Map();
-  for (const stone of stones) map.set(stone, new Set());
-  map.get(0).add(0);
-  for (let i = 0; i < stones.length; i++) {
-    for (const step of map.get(stones[i])) {
-      for (let k = step - 1; k <= step + 1; k++) {
-        if (k > 0 && map.has(stones[i] + k)) map.get(stones[i] + k).add(k);
+const quickSortRandomPivot = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const pivotIndex = Math.floor(Math.random() * arr.length);
+  const pivot = arr[pivotIndex];
+  const left = [];
+  const right = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== pivotIndex) {
+      if (arr[i] <= pivot) {
+        left.push(arr[i]);
+      } else {
+        right.push(arr[i]);
       }
     }
   }
-  return map.get(stones[stones.length - 1]).size > 0;
-}
+  return quickSortRandomPivot(left).concat(pivot, quickSortRandomPivot(right));
+};
